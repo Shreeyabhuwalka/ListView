39e918dd41cf1615692d43f08d3ad32fcc52524f
Shreeya <shreeyabhuwalka@gmail.com>
2020-09-13 19:46:15 +0530

    Initial commit

---
+++ app/src/main/java/com/developer/listview/MainActivity.java
@@ -0,0 +1,68 @@
+package com.developer.listview;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView list;
+    String mText[] = {"Facebook","Whatsapp","Twitter"};
+    String dec[]={"facebook des1","whatsapp des","twitter des"};
+    int image[]={R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.listView);
+        MyAdapter adapter = new MyAdapter(this,mText,dec,image);
+        list.setAdapter(adapter);
+    }
+
+    class MyAdapter extends ArrayAdapter<String>
+    {
+        Context mContext;
+        String rTitle[];
+        String rDes[];
+        int rImag[];
+
+
+        MyAdapter(Context c,String title[],String des[], int img[])
+        {
+            super(c,R.layout.row,R.id.name,title);
+            this.mContext = c;
+            this.rTitle = title;
+            this.rDes = des;
+            this.rImag = img;
+        }
+
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row=layoutInflater.inflate(R.layout.row,parent,false);
+            ImageView images = row.findViewById(R.id.image);
+            TextView myTitle = row.findViewById(R.id.name);
+            TextView myDes = row.findViewById(R.id.description);
+
+            images.setImageResource(rImag[position]);
+            myTitle.setText(rTitle[position]);
+            myDes.setText(rDes[position]);
+
+            return row;
+
+        }
+    }
+}
